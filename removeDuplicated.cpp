44deebc54f8eaf5db3bf4474400a9e89a66f31c6
JUAN M MOTA GONZALEZ <mike.mota@gmail.com>
2021-08-24 21:04:24 -0500

    another one
    ....

---
+++ removeDuplicated.cpp
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        if(nums.size()>0){
+            int i = 1;
+            int lastVal=nums[0];
+            while(i<nums.size()){
+                
+                if(nums[i]==lastVal){
+                    nums.erase(nums.begin()+i);
+                }
+                else{
+                    lastVal=nums[i++];
+                }
+            }
+        }
+        
+        return nums.size();
+    }
+};
